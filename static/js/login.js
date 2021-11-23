4f85e395e0b3b5fb2a13095c12abc0f40b831ad0
hyeongjunkim <hjthelegend@naver.com>
2021-11-24 01:00:12 +0900

    1123 Add [profile innerThings and search]

---
+++ static/js/login.js
@@ -0,0 +1,17 @@
+// let id = $('.id');
+// let pw = $('.pw');
+// let btn = $('.btn')
+// console.log(id, pw, btn)
+// $(btn).on('click', function() {
+//   if($(id).val() == '') {
+//     $(id).next('label').addClass('warning');
+//     setTimeout(function() {
+//       $('label').removeClass('warning');
+//     }, 1500)
+//   } else if ($(pw).val() =='') {
+//     $(pw).next('label').addClass('warning');
+//     setTimeout(function() {
+//       $('label').removeClass('warning');
+//     }, 1500)
+//   }
+// });
